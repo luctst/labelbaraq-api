9763d165b5d25703547c6118397003abbc4c24ed
luctst <lucas.tostee@gmail.com>
2020-10-24 15:16:53 +0200

    Init commit let's go git add .

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5a5efa1104073d47412ffbfc6c2346a6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b742f89f-c177-4d14-8517-9ad9919b4e56'
+};
