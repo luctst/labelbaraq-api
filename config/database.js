a45d74368a933818adec686303d2c67280ae4c43
luctst <lucas.tostee@gmail.com>
2020-10-24 15:57:34 +0200

    Update strapi config

--- config/database.js
+++ config/database.js
@@ -5,6 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
