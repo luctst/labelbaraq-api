3055cdd9b2bf1d014cefa22fec4e0d3130d56377
luctst <lucas.tostee@gmail.com>
2020-11-09 15:40:42 +0100

    Add env var with process.env object

--- config/plugins.js
+++ config/plugins.js
@@ -1,31 +1,34 @@
-module.exports = ({ env }) => ({
-    // ...
-    email: {
-        provider: 'sendgrid',
-        providerOptions: {
-            apiKey: env('SENDGRID_API_KEY'),
+module.exports = ({ env }) => {
+    console.log(env('CLOUDINARY_KEY'));
+    return {
+        // ...
+        email: {
+            provider: 'sendgrid',
+            providerOptions: {
+                apiKey: env('SENDGRID_API_KEY'),
+            },
+            settings: {
+                defaultFrom: 'lucas.tostee@gmail.com',
+                defaultReplyTo: 'lucas.tostee@gmail.com',
+            },
         },
-        settings: {
-            defaultFrom: 'lucas.tostee@gmail.com',
-            defaultReplyTo: 'lucas.tostee@gmail.com',
+        upload: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: process.env.CLOUDINARY_NAME,
+                api_key: process.env.CLOUDINARY_KEY,
+                api_secret: process.env.CLOUDINARY_SECRET,
+            },
+            // provider: 'aws-s3',
+            // providerOptions: {
+            //     accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            //     secretAccessKey: env('AWS_ACCESS_SECRET'),
+            //     region: 'eu-west-1',
+            //     params: {
+            //         Bucket: 'label-baraq',
+            //     },
+            // },
         },
-    },
-    upload: {
-        provider: 'cloudinary',
-        providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET'),
-        },
-        // provider: 'aws-s3',
-        // providerOptions: {
-        //     accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        //     secretAccessKey: env('AWS_ACCESS_SECRET'),
-        //     region: 'eu-west-1',
-        //     params: {
-        //         Bucket: 'label-baraq',
-        //     },
-        // },
-    },
-    // ...
-});
+        // ...
+    }
+};
